57909a43b47f657e2319e5ba3620057f815cfbf3
Gracie Canfield <graciecanfield@gracies-mbp.wireless.rit.edu>
2018-12-13 13:04:28 -0500

    comments

--- fall2018makeup/fall2018finalmakeup.js
+++ fall2018makeup/fall2018finalmakeup.js
@@ -15,12 +15,12 @@ $(".menu").click(function() {
     $(this).next(".submenu").toggle();
 });
 
-//starting story and story selector
-let start = ("#story0.txt");
-$("#submenu").val(start); //changes menu option to default
-$("#story").load(start);   //retrieves only default element
+//starting story and story selector - my attempt - every time i tried to look at the site when I added this, it bugged out
+//let start = ("#story0.txt");
+//$("#submenu").val(start); //changes menu option to default
+//$("#story").load(start);   //retrieves only default element
 
-$("#submenu").change(function() {
-    story = $(this).val();
-    $("#story").load(start); 
-});
+//$("#submenu").change(function() {
+//    story = $(this).val();
+//    $("#story").load(start); 
+//});
